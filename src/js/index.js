80709d6a1e5e387417264aa3bf335d7a8f3302f2
Jingwei Wang <wang-jing-wei@hotmail.com>
2022-08-29 00:40:39 -0500

    mp0

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,44 @@
 /* Your JS here. */
 console.log('Hello World!')
+window.onload = function(){
+
+    var redBtn = document.getElementById('red');
+    var blueBtn = document.getElementById('blue');
+    var greyBtn = document.getElementById('grey');
+    var btn = document.getElementsByClassName('container');
+    var phoImg = document.getElementById('pho');
+    var nameH = document.getElementById('name');
+    
+    redBtn.onclick = function(){
+    
+        btn[0].style.backgroundColor = 'brown';
+    
+    };
+
+    blueBtn.onclick = function(){
+    
+        btn[0].style.backgroundColor = 'plum';
+    
+    };
+
+    greyBtn.onclick = function(){
+    
+        btn[0].style.backgroundColor = 'grey';
+    
+    };
+
+    phoImg.onclick = function(){
+    
+        document.getElementById("bio").style.display = "none";
+    
+    };
+
+    nameH.onclick = function(){
+    
+        document.getElementById("bio").style.display = "block";
+    
+    };
+    
+    };
+    
+    
